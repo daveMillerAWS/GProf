a04bea77da89de8e7b20c82ae9f46a190147f05a
Dave Miller <miller.dave0219@gmail.com>
2020-05-24 04:35:25 -0500

    8

--- case_study/Case_study2.sh
+++ case_study/Case_study2.sh
@@ -14,7 +14,7 @@ function passwordChecker { #compare typed password with hashed one in the system
     userHashedPassword=$(sudo cat /etc/shadow | grep -i $user | cut -d":" -f2)
     hashType=$(echo $userHashedPassword | cut -d"$" -f2) #Get the hash function type
     salt=$(sudo cat /etc/shadow | grep -i $user | cut -d"$" -f3) #Get the salt for hash function
-    hashed_password=$(openssl passwd -$hashType -salt $salt  $password) #hash user password
+    hashed_password=$(openssl passwd -$hashType -salt $salt  $password) || $(openssl11 passwd -$hashType -salt $salt  $password) #hash user password (AMAZON uses openssl11)
     printf $userHashedPassword
     printf $hashed_password
 
@@ -61,7 +61,7 @@ then
          
     else #     If Python is not installed, install it within your script
          echo -e "!!!!  Python3 has not installed yet"
-         sudo apt install python3.8 || yum install python3.8
+         sudo apt install python3.8 || sudo yum install python3
         
         if [[ $(find ./ -name "phone_book.py" | wc -l) -gt 0 ]]; # if file exists line number should 1, so TRUE
         then
--- case_study/readme.md
+++ case_study/readme.md
@@ -1,4 +1,7 @@
 ################## CASE STUDY 2 ##############
 !!! IMPORTANT !!!
-change package manager code in Case_study_2.sh depending on your OS distros
-by default apt is used 
+verify before start script
+
+- set ec2-user password
+- install git 
+
